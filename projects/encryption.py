e07db25c679b5783ab31d282baacfde6307f0762
Dax1386 <daxvarasadiya@gmail.com>
2026-09-13 18:50:03 +0530

    encryption

---
+++ projects/encryption.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+char = " " + string.punctuation + string.digits + string.ascii_letters
+char = list(char)
+key = char.copy()
+
+random.shuffle(key)
+print(key)
+
+#ENCRYPT
+plain_text = input("enter the plain_text")
+cryption_text = ""
+
+for letter in plain_text :
+    index = plain_text.index(letter)
+    cryption_text += key[index]
+
+print(cryption_text)
